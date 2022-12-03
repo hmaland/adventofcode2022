b485e44184b1cd956b5a24470cba4213fba1e7f9
Henning Maeland <hmaland@users.noreply.github.com>
2022-12-03 20:04:18 +0100

    Advent of code 2022 - day 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using adventofcode2022;
+
+Console.WriteLine("hmaland's Advent of Code 2022 answers");
+
+Console.WriteLine("Day 1 - Calorie Counting - Part 1: " + new Day1().Part1_Max());
+Console.WriteLine("Day 1 - Calorie Counting - Part 2: " + new Day1().Part2_Top3());
+
+Console.ReadLine();
+
+
---
+++ day1/Day1.cs
@@ -0,0 +1,45 @@
+﻿namespace adventofcode2022
+{
+    internal class Day1
+    {
+        public int Part1_Max()
+        {
+            var input = File.ReadAllLines(@"day1\input.txt");
+            var max = 0;
+            var sum = 0;
+            foreach(var line in input)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    max = Math.Max(max, sum);
+                    sum = 0;
+                }
+                else
+                {
+                    sum += int.Parse(line);
+                }
+            }
+            return max;
+        }
+
+        public int Part2_Top3()
+        {
+            var input = File.ReadAllLines(@"day1\input.txt");
+            var elfCalories = new List<int>();
+            int sum = 0;
+            foreach (var line in input)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    elfCalories.Add(sum);
+                    sum = 0;
+                }
+                else
+                {
+                    sum += int.Parse(line);
+                }
+            }
+            return elfCalories.OrderByDescending(x => x).Take(3).Sum();
+        }
+    }
+}
